4740d2b8aa41df4d65978dd028c52958dcd2f334
Joe Yan <chengmu.yan@gmail.com>
2020-04-14 17:53:21 -0400

    Initialize db and model

--- qa-server/model.js
+++ qa-server/model.js
@@ -1,1 +1,19 @@
 const db = require('./db.js');
+
+module.exports = {
+  getQuestionsByProduct: function (productId) {},
+
+  getAnswersByQuestion: function (questionId) {},
+
+  postQuestion: function () {},
+
+  postAnswer: function () {},
+
+  markAnswerHelpful: function (answerId) {},
+
+  reportAnswer: function (answerId) {},
+
+  markQuestionHelpful: function (questionId) {},
+
+  reportQuestion: function (questionId) {},
+};
